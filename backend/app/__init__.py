3ba0c634c80f91f4ef65f4322ff5f4582a0c76b3
carmial65 <carmial65@gmail.com>
2025-07-04 11:17:54 +0200

    inicializar SQLAlchemy and Migrate en  create_app()

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,19 @@
 # backend/app/__init__.py
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from .config.setting import DevelopmentConfig
 from .routes.tasks import tasks_bp          
+
+db = SQLAlchemy()
+migrate = Migrate()     
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(DevelopmentConfig)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     app.register_blueprint(tasks_bp, url_prefix='/api')  
 
