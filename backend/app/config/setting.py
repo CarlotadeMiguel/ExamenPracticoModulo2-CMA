c97e932125d4c72cb33b0a6eae30a21a54be2533
carmial65 <carmial65@gmail.com>
2025-07-04 11:31:59 +0200

    config

--- backend/app/config/setting.py
+++ backend/app/config/setting.py
@@ -1,3 +1,14 @@
 #backend/app/config/setting.py
-class DevelopmentConfig:
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'dev-key')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URI',
+        'sqlite:///' + os.path.join(basedir, '..', 'instance', 'app.db')
+    )
