3f2dd358caa30cd88f3ea03cb68d8f65d1020ed1
carmial65 <carmial65@gmail.com>
2025-07-04 11:30:03 +0200

    Carga variables de entorno

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,19 +2,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from .config.setting import DevelopmentConfig
-from .routes.tasks import tasks_bp          
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
-migrate = Migrate()     
+migrate = Migrate()
 
 def create_app():
+    load_dotenv(dotenv_path=os.path.join(os.getcwd(), '.env'))
+
     app = Flask(__name__)
-    app.config.from_object(DevelopmentConfig)
+    app.config.from_object(os.getenv('FLASK_ENV') == 'development'
+        and "app.config.setting.DevelopmentConfig"
+        or "app.config.setting.Config"
+    )
 
     db.init_app(app)
     migrate.init_app(app, db)
 
-    app.register_blueprint(tasks_bp, url_prefix='/api')  
+    from .routes.tasks import tasks_bp
+    app.register_blueprint(tasks_bp, url_prefix='/api')
 
     return app
